943b6895feae9188c1515bf54ab0ed4f21414306
SkynetBuilder <vlad.gulenkov@gmail.com>
2023-12-13 20:37:13 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/calculatorwithtests/CalculatorWithTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.calculatorwithtests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorWithTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
