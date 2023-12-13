cff1038675225f54f3bf452816a70acae796dc7c
SkynetBuilder <vlad.gulenkov@gmail.com>
2023-12-13 22:42:17 +0300

    Задание выполнено

---
+++ src/main/java/pro/sky/java/course2/calculatorwithtests/service/CalculatorService.java
@@ -0,0 +1,11 @@
+package pro.sky.java.course2.calculatorwithtests.service;
+
+public interface CalculatorService {
+    Integer plus(int num1, int num2);
+
+    Integer minus(int num1, int num2);
+
+    Integer multiply(int num1, int num2);
+
+    Integer divide(int num1, int num2);
+}
